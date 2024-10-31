985913496344147013e98c39ae437963e4783c04
MOISES GUZMAN MORA <moisomor22@gmail.com>
2024-10-30 21:42:24 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Examen1
+{
+    internal class Program
+    {
+
+        private static int numero = 24;
+
+        static void Main(string[] args)
+        {
+
+            HiloHexadecimal.Start();
+            HiloBinario.Start();
+            HiloOctal.Start();
+
+            Console.ReadLine();
+        }
+
+        private static void Hexadecimal()
+        {
+
+        }
+        private static void Binario()
+        {
+
+        }
+        private static void Octal()
+        {
+
+        }
+
+        public static Thread HiloHexadecimal = new Thread(Hexadecimal);
+        public static Thread HiloBinario = new Thread(Binario);
+        public static Thread HiloOctal = new Thread(Binario);
+
+    }
+}
