4d78b7e8e6006c45d594e5f135f836d1f17f943e
MOISES GUZMAN MORA <moisomor22@gmail.com>
2024-10-30 22:12:28 -0600

    se han arreglado errores y he probado los bloqueos

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ namespace Examen1
     {
 
         private static int numero = 24;
+        private static readonly object bloqueo = new object();
+
 
         static void Main(string[] args)
         {
@@ -23,20 +25,40 @@ namespace Examen1
 
         private static void Hexadecimal()
         {
+            lock (bloqueo)
+            {
 
+                Console.WriteLine("convirtiendo a hexadecimal");
+                Console.WriteLine( "resultado en hexadecimal:"+numero);
+                Thread.Sleep(5000);
+
+            }
         }
         private static void Binario()
         {
+            lock (bloqueo)
+            {
+                Console.WriteLine("convirtiendo a Binario");
+                Console.WriteLine("resultado en Binario:"+numero);
+                Thread.Sleep(5000);
 
+
+            }
         }
         private static void Octal()
         {
+            lock (bloqueo)
+            {
+                Console.WriteLine("convirtiendo a Octal");
+                Console.WriteLine("resultado en Octal:"+numero);
+                Thread.Sleep(5000);
 
+            }
         }
 
         public static Thread HiloHexadecimal = new Thread(Hexadecimal);
         public static Thread HiloBinario = new Thread(Binario);
-        public static Thread HiloOctal = new Thread(Binario);
+        public static Thread HiloOctal = new Thread(Octal);
 
     }
 }
